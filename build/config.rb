8587006d41d6a4d514b5e513d8f15e6c82cdd25c
Alexander Zizzo <elzi@nerdi.net>
2014-08-05 12:32:31 -0700

    relocate gemfile

--- build/config.rb
+++ build/config.rb
@@ -2,11 +2,11 @@
 
 # Set this to the root of your project when deployed:
 http_path       = "/"
-css_dir         = "assets/css"
-sass_dir        = "assets/sass"
-images_dir      = "assets/images"
-javascripts_dir = "assets/js"
-fonts_dir       = "assets/fonts"
+css_dir         = "../assets/css"
+sass_dir        = "../assets/sass"
+images_dir      = "../assets/images"
+javascripts_dir = "../assets/js"
+fonts_dir       = "../assets/fonts"
 
 
 # You can select your preferred output style here (can be overridden via the command line):
