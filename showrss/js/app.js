24ac6b9315135add1a0435aa33882f164e3ee6e5
Alexander Zizzo <elzi@nerdi.net>
2015-02-03 17:15:36 -0800

    complete-ish

--- showrss/js/app.js
+++ showrss/js/app.js
@@ -34,8 +34,13 @@ var APP = (function () {
 
     },
 
-    feeds : {
+    dir : {
+      js : window.location.href + 'js/',
+      css : window.location.href + 'css/',
+      images : window.location.href + 'images/',
+    },
 
+    feeds : {
       showrss_all : 'http://showrss.info/rss.php?user_id=207854&hd=0&proper=null'
     }
 
@@ -126,27 +131,25 @@ var APP = (function () {
           // Get cleaned up torrent info
           var show_info = _this.filterShowTorrentInfo( show.link, show.title )   
 
-          // Magnet URI
-          var magnet_uri = show.link;
-
           // Format date
           var release_date = formatPubDate( show.pubDate )
+          var release_day_of_week = formatPubDate( show.pubDate, 'day' )
 
-          console.log('Show Torrent Info: ', show_info)
           
+          var cover_image_src = app.dir.images + show_info.slug + '.png';
 
           app.$el.shows.append('\
             <div class="show col-md-3"> \
-              <div class="show__inner"> \
-                <div class="show__info"> \
-                  <h4 class="show-title">' + show_info.title + '</h4> \
-                </div> \
+              <div class="show__info"> \
+                <h4 class="show-title">' + show_info.title + ' ' + show_info.sxe + '</h4> \
+                <h6><b>' + release_day_of_week + '</b> - ' + release_date + '</h6> \
+                <a href="' + show.link + '"><i class="show__play fa fa-play-circle"></i></a> \
               </div> \
+              <div class="show__cover-photo" style="background-image:url(\'' + cover_image_src + '\')"></div> \
             </div> \
           ')
 
-          
-
+          console.log('Show Torrent Info: ', show_info)
         })
 
       })
@@ -161,38 +164,47 @@ var APP = (function () {
      * Use regex on torrent file name to return some cleaned up strings
      */
     filterShowTorrentInfo: function( show_link, show_title ) {
+ 
+      var torrent_info = {};
 
-       var torrent_info = {};
+      if ( show_link ) {
+        // Get torrent filename
+        var torrent_str = getTorrentFilenameFromMagnetURI( show_link )
 
-       if ( show_link ) {
-         // Get torrent filename
-         var torrent_str = getTorrentFilenameFromMagnetURI( show_link )
+        // Nicename
+        var show_nicename       = torrent_str.match(/^(\[.+\])?(.+[^a-z0-9])(?=S\d)/)[0]
+            show_nicename       = show_nicename.replace(/\+/g, ' ').replace(' US', '').trim()
+            torrent_info.title   = show_nicename.replace(/\d{4}$/g, '').trim()
 
-         // Nicename
-         var show_nicename         = torrent_str.match(/^(\[.+\])?(.+[^a-z0-9])(?=S\d)/)
-             torrent_info.title    = show_nicename[0].replace(/\+/g, ' ').trim()
+        // Slug
+        torrent_info.slug       = generateSlug(show_nicename)
 
-         // Season
-         var show_season         = torrent_str.match(/S(\d{1,2})/)
-             torrent_info.season = show_season[0].replace('S', '')
+        // Season
+        var show_season         = torrent_str.match(/S(\d{1,2})/)[0]
+           torrent_info.season  = show_season.replace('S', '')
 
-         // Episode
-         var show_episode         = torrent_str.match(/E(\d{1,2})/)
-             torrent_info.episode = show_episode[0].replace('E', '')
-       } else {
-         console.log('ERROR: show_link not passed')
-       }
+        // Episode
+        var show_episode        = torrent_str.match(/E(\d{1,2})/)[0]
+           torrent_info.episode = show_episode.replace('E', '')
+      } else {
+        console.log('ERROR: show_link not passed')
+      }
 
-       // Episode Name
-       if ( show_title ) {
-         var show_sxe = show_title.match(/\d{1,2}x\d{1,2}/)
-             show_sxe = show_sxe[0]
+      // Episode Name
+      if ( show_title ) {
+        var show_sxe = show_title.match(/\d{1,2}x\d{1,2}/)
+            show_sxe = show_sxe[0]
 
-         var show_episode_name = show_title.substring( show_title.indexOf(show_sxe) )
-             show_episode_name = show_episode_name.replace(show_sxe, '')
-             show_episode_name = show_episode_name.replace(/[-][\d].[\s]/, '').trim()
+        // SxE
+        torrent_info.sxe = show_sxe
 
-         torrent_info.episode_name = show_episode_name
+        // Episode Name
+        var show_episode_name = show_title.substring( show_title.indexOf(show_sxe) )
+            show_episode_name = show_episode_name.replace(show_sxe, '')
+            show_episode_name = show_episode_name.replace(/[-][\d].[\s]/, '').trim()
+
+        torrent_info.episode_name = show_episode_name
+
        } else {
          console.log('ERROR: show_title not passed')
        }
@@ -200,7 +212,7 @@ var APP = (function () {
 
        return torrent_info;
 
-    }
+    },
 
 
   },
@@ -297,15 +309,59 @@ var APP = (function () {
 
   }
 
-   /**
-    * Format Published Date
-    */
-  function formatPubDate( date_str ) {
+  /**
+   * Format Published Date
+   */
+  function formatPubDate( date_str, format ) {
+    
+    var format = ( format !== undefined ) ? format : 'full'
+
+    var date = new Date(date_str)
+
+    if ( format == 'full' ) {
+
+      return date.toLocaleDateString()
+
+    } else if ( format == 'day' ) {
+      
+      var day = date.getDay()
+      return ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"][day]
+
+    } else {
+      return date
+    }
+  }
+
+  /**
+   * Format Published Date to Day of Week
+   */
+  function pubDateToDayOfWeek( date_str ) {
     
     var date = new Date(date_str)
 
-    return date.toLocaleDateString()
+    return date.getDay()
   }
+
+  /**
+  * Converts a day number to a string.
+  *
+  * @method dayOfWeekAsString
+  * @param {number} day_index
+  * @return {Number} Returns day as number
+  */
+  function dayOfWeekAsString(day_index) {
+    return ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"][day_index];
+  }
+
+  /**
+   * Generate Slug
+   *
+   * convert to lowercase, remove dashes and pluses, replace spaces with dashes
+   * remove everything but alphanumeric characters and dashes
+   */
+  function generateSlug (value) {
+    return value.toLowerCase().replace(/-+/g, '').replace(/\s+/g, '-').replace(/[^a-z0-9-]/g, '')
+  };
 
 
 
